ee0879614f96dda5f9ab533746d5f7481921cd2f
CredoM <942794848@qq.com>
2018-05-27 19:30:53 +0800

    1.0测试bug15

--- src/components/Answer1.vue
+++ src/components/Answer1.vue
@@ -237,6 +237,7 @@ export default {
                     })
                     .catch((err) => {
                         console.log(err);
+                        alert(err);
                     });
         }
     }
